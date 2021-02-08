9c507548542437cf84c51b64d7070c8eae9a6dc9
ChiruZy <2934262493@qq.com>
2021-02-08 16:22:52 +0800

    添加对voc数据集的先验框的 k-means聚类

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import xml.dom.minidom
 
 
 def read_train_val():
@@ -27,8 +29,10 @@ def read_train_val():
 
         with open('train.txt', 'a') as f:
             for name in res:
-                f.write(''.join([ann, '/', name, '.xml', ' ',
-                                 img, '/', name, '.jpg', '\r\n']))
+                a_path = ''.join([ann, '/', name, '.xml'])
+                i_path = ''.join([img, '/', name, '.jpg'])
+                if os.path.exists(a_path) and os.path.exists(i_path):
+                    f.write(''.join([a_path, ' ', i_path, '\r\n']))
 
 
 def read_test():
@@ -46,14 +50,76 @@ def read_test():
 
     with open('test.txt', 'a') as f:
         for name in res:
-            f.write(''.join([ann_path, '/', name, '.xml', ' ',
-                             img_path, '/', name, '.jpg', '\r\n']))
+            a_path = ''.join([ann_path, '/', name, '.xml'])
+            i_path = ''.join([img_path, '/', name, '.jpg'])
+            if os.path.exists(a_path) and os.path.exists(i_path):
+                f.write(''.join([a_path, ' ', i_path, '\r\n']))
+
+
+def get_all_wh(txt, img_size=416):
+    ann_list = []
+    with open(txt, 'r') as f:
+        for line in f:
+            ann_list.append(line.split(' ')[0])
+
+    all_size = []
+    for ann in ann_list:
+        DOMTree = xml.dom.minidom.parse(ann)
+        annotation = DOMTree.documentElement
+        size = annotation.getElementsByTagName('size')[0]
+        w = int(size.getElementsByTagName('width')[0].childNodes[0].data)
+        h = int(size.getElementsByTagName('height')[0].childNodes[0].data)
+        ratio = max(w, h) / img_size
+
+        objs = annotation.getElementsByTagName("object")
+        for obj in objs:
+            bbox = obj.getElementsByTagName('bndbox')[0]
+            xmin = int(bbox.getElementsByTagName('xmin')[0].childNodes[0].data)
+            xmax = int(bbox.getElementsByTagName('xmax')[0].childNodes[0].data)
+            ymin = int(bbox.getElementsByTagName('ymin')[0].childNodes[0].data)
+            ymax = int(bbox.getElementsByTagName('ymax')[0].childNodes[0].data)
+
+            obj_w = (xmax - xmin) / ratio
+            obj_h = (ymax - ymin) / ratio
+            all_size.append((obj_w, obj_h))
+    return all_size
+
+
+def iou(size1, size2):
+    union = np.minimum(size1[:, 0], size2[0]) * np.minimum(size1[:, 1], size2[1])
+    intersection = size1[:, 0] * size1[:, 1] + size2[0] * size2[1] - union + 1e-10
+    return union / intersection
+
+
+def anchor_box_k_mean(img_size=416, k=9):
+    all_size = get_all_wh('train.txt', img_size)
+
+    res = np.array(all_size)
+    rows = res.shape[0]
+    clusters = res[np.random.choice(rows, k, replace=False)]
+
+    distance = np.zeros((rows, k))
+    last = np.zeros((rows,))
+
+    while True:
+        for i in range(k):
+            distance[:, i] = 1 - iou(res, clusters[i])
+        current = np.argmin(distance, axis=1)
+
+        if all(current == last):
+            break
+
+        last = current
+        for i in range(k):
+            clusters[i, :] = np.median(res[last == i], axis=0)
+
+    return clusters
 
 
 if __name__ == '__main__':
-    read_train_val()
-    read_test()
-
+    # read_train_val()
+    # read_test()
+    anchor_box_k_mean(416, 9)
 
 
 
