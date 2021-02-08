10282726af0c4d766a68f1fb9798565e1992aba5
ChiruZy <2934262493@qq.com>
2021-02-08 19:59:43 +0800

    添加类名转换为label的函数

---
+++ tools.py
@@ -0,0 +1,3 @@
+def name_to_label(name):
+    return ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car' ,'cat' ,'chair', 'cow','diningtable',
+     'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'].index(name)
